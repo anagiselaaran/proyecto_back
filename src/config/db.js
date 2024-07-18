aa2f928d5b0825d050f252bad083d2954a10504b
luis cardenes <luiscardenes21@gmail.com>
2024-07-18 08:39:30 +0100

    creamos carpeta config para conectar con la base de datos

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+
+const pool = mysql.createPool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+})
+
+// variable global para usar sobre las queries
+global.db = pool.promise();
