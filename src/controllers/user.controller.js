5b52303348cb9a3d3708bc5faa9e8d08cd3f756c
luis cardenes <luiscardenes21@gmail.com>
2024-07-22 08:28:02 +0100

    usermodel delete user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const User = require('../models/user.model');
-
+//peticion para obtener todos los usuarios
 const getUsers = async (req, res) => {
   
     try {
@@ -9,7 +9,7 @@ const getUsers = async (req, res) => {
         res.status(500).json({message: error.message})
     }
 }
-
+//peticion para crear usuario
 const createUser = async (req, res) => {
     try {
         const [send] = await User.insert(req.body);
@@ -20,7 +20,7 @@ const createUser = async (req, res) => {
 
     }
 }
-
+//peticion para actualizar datos del usuario
 const updateUser = async (req, res) => {
     const { userId } = req.params;
     try {
@@ -32,9 +32,21 @@ const updateUser = async (req, res) => {
 
     }
 }
+//peticion para borrar un usuario
+const deleteUser = async (req, res) => {
+    const { userId } = req.params;  
+    try {
+        const [users] = await User.SelectById(userId)
+        const [response] = await User.deleteId(userId)
+        res.json(users[0]);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
 
 module.exports = {
     getUsers,
     createUser,
-    updateUser
+    updateUser,
+    deleteUser
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,33 +1,39 @@
 
+//seleccionar todos los usuarios
 const selectAll = () => {
     return db.query('select * from users')
 }
-
+//seleccionar usuario por id
 const SelectById = (userId) => {
     return db.query('select * from users where id = ?', [userId])
 }
-
-/* const insert = ({ name, surname, email, role, department, contracted_hours, is_active }) => {
+//crear un nnuevo usuario en la base de datos
+const insert = ({ name, surname, email, role, department, contracted_hours, is_active }) => {
 
     return db.query('insert into users (name, surname, email, role, department, contracted_hours, is_active )  values (?, ?, ?, ?, ?, ?, ?)',
         [name, surname, email, role, department, contracted_hours, is_active]);
-} */
-//prueba
-const insert = ({ name, surname, email, password, department, contracted_hours }) => {
-
-    return db.query('insert into users (name, surname, email, password, department, contracted_hours )  values (?, ?, ?, ?, ?, ?)',
-        [name, surname, email, password, department, contracted_hours]);
 }
+//prueba
+/* const insert = ({ name, surname, email, password, role, department, contracted_hours, is_active }) => {
 
+    return db.query('insert into users (name, surname, email, password, role, department, contracted_hours, is_active )  values (?, ?, ?, ?, ?, ?, ?, ?)',
+        [name, surname, email, password, role, department, contracted_hours, is_active]);
+} */
 
+//actualizar usuario
 const updateById = (userId, { name, surname, email, role, department, contracted_hours, is_active }) => {
     return db.query('update users set name = ?, surname = ?, email = ?, role = ?, department = ?, contracted_hours =?, is_active = ? where id =?',
         [name, surname, email, role, department, contracted_hours, is_active, userId]);
+}
+//borrar usuario
+const deleteId = (userId) => {
+    return db.query('delete from users where id = ?', [userId])
 }
 
 module.exports = {
     selectAll,
     SelectById,
     insert,
-    updateById
+    updateById,
+    deleteId
 }
