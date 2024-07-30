ea6d00505001c409ce75571a248168f13f4b3a28
André Candeloro <candeloro95@gmail.com>
2024-07-30 16:03:29 +0200

    Added checkToken middleware to time routes.

--- src/routes/api/times.js
+++ src/routes/api/times.js
@@ -13,26 +13,27 @@ const {
     updateByUserIdAndDate,
     deleteByUserIdAndDate,
 } = require("../../controllers/time.controller");
+const { checkToken } = require("../../utils/middlewares");
 
 // Requests
 
 // GET
 // TODO: Change all routes that require userId to use req.user.id
-router.get("/", getAll);
-router.get("/user/:userId", getByUserId);
-router.get("/date/:date", getByDate);
-router.get("/user/:userId/date/:date", getByUserIdAndDate);
-router.get("/start/:start/end/:end", getByPeriod);
-router.get("/user/:userId/start/:start/end/:end", getByUserIdAndPeriod);
+router.get("/", checkToken, getAll);
+router.get("/user/:userId", checkToken, getByUserId);
+router.get("/date/:date", checkToken, getByDate);
+router.get("/user/:userId/date/:date", checkToken, getByUserIdAndDate);
+router.get("/start/:start/end/:end", checkToken, getByPeriod);
+router.get("/user/:userId/start/:start/end/:end", checkToken, getByUserIdAndPeriod);
 
 // POST
-router.post("/new", createTime);
-router.post("/project/new", createProjectTime);
+router.post("/new", checkToken, createTime);
+router.post("/project/new", checkToken, createProjectTime);
 
 // PUT
-router.put("/edit/user/:userId", updateByUserIdAndDate);
+router.put("/edit/user/:userId", checkToken, updateByUserIdAndDate);
 
 // DELETE
-router.delete("/user/:userId/date/:date", deleteByUserIdAndDate);
+router.delete("/user/:userId/date/:date", checkToken, deleteByUserIdAndDate);
 
 module.exports = router;
